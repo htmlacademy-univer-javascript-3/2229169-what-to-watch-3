4d8cb1c7a766a2ff402b77b40b5a0c508d6a4786
Artur Imankulov <arturimankulov906@gmail.com>
2023-12-24 12:22:34 +0500

    fix test

--- src/store/middlewares/redirect.test.ts
+++ src/store/middlewares/redirect.test.ts
@@ -1,20 +1,14 @@
-import { MockStore, configureMockStore } from '@jedmao/redux-mock-store';
+import { assert } from 'chai';
+import { configureMockStore, MockStore } from '@jedmao/redux-mock-store';
 import { redirect } from './redirect';
-import * as browserHistory from '../../services/browser-history'; // Используем * as для импорта
+import * as browserHistory from '../../services/browser-history';
 import { AnyAction } from '@reduxjs/toolkit';
 import { redirectToRoute } from '../action';
 import { AppRoute } from '../../const';
 import { State } from '../../types/state';
 
-jest.mock('../../services/browser-history', () => ({
-  location: { pathname: '' },
-  push(path: string) {
-    this.location.pathname = path;
-  }
-}));
-
 describe('Redirect middleware', () => {
-  let store: MockStore<State, AnyAction>; // Укажем типы для store
+  let store: MockStore<State, AnyAction>;
 
   beforeAll(() => {
     const middleware = [redirect];
@@ -23,27 +17,27 @@ describe('Redirect middleware', () => {
   });
 
   beforeEach(() => {
-    const history = browserHistory as { default?: { push: (path: string) => void } }; // Уточняем тип history
+    const history = browserHistory as { default?: { push: (path: string) => void } };
     if (history.default && typeof history.default.push === 'function') {
       history.default.push('');
     }
   });
 
   it('should redirect to "/login" with redirectToRoute action', () => {
-    const history = browserHistory as { default?: { location?: { pathname: string } } }; // Уточняем тип history
+    const history = browserHistory as { default?: { location?: { pathname: string } } };
     if (history.default && history.default.location && typeof history.default.location.pathname === 'string') {
       const redirectAction = redirectToRoute(AppRoute.SignIn);
       store.dispatch(redirectAction);
-      expect(history.default.location.pathname).toBe(AppRoute.SignIn);
+      assert.strictEqual(history.default.location.pathname, AppRoute.SignIn);
     }
   });
 
   it('does not redirect to "/main" page with empty action', () => {
-    const history = browserHistory as { default?: { location?: { pathname: string } } }; // Уточняем тип history
+    const history = browserHistory as { default?: { location?: { pathname: string } } };
     if (history.default && history.default.location && typeof history.default.location.pathname === 'string') {
       const emptyAction = { type: '', payload: AppRoute.Main };
       store.dispatch(emptyAction);
-      expect(history.default.location.pathname).not.toBe(AppRoute.Main);
+      assert.notStrictEqual(history.default.location.pathname, AppRoute.Main);
     }
   });
 });
