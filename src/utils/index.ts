51cc1b0f0d8ace1fceabf3e5dfcbe2389d6a1234
Artur Imankulov <arturimankulov906@gmail.com>
2023-12-24 11:54:05 +0500

    8.13

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-export * from './change-date-format/change-date-format';
+export * from './change-data-format/change-date-format';
 export * from './get-run-time/get-run-time';
 export * from './get-text-representation/get-text-representation-rating';
 export * from './get-time-left/get-time-left';
